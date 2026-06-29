69303f877f567de68595575542ceb3d9f23f6ac7
L123121 <L123121@users.noreply.github.com>
2026-06-29 11:00:14 +0800

    chore: 从git追踪移除cookies.txt和design-archive，更新.gitignore

--- src/design-archive/backend/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const authController = require('../controllers/auth.controller');
-
-const router = Router();
-router.post('/login', authController.login);
-
-module.exports = router;
